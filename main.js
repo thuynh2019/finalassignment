2a8add5eea6b555261535323ce5d18139f5bcdee
thuynh2019 <ty.huynh@smail.astate.edu>
2020-02-26 21:33:42 -0600

    final maybe

--- main.js
+++ main.js
@@ -21,6 +21,11 @@ btn.onclick = function() {
   const btnClass = btn.getAttricute('class');
   if(btnClass === 'dark') {
     btn.setAttribute('class', 'light');
-
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
   }
 }
